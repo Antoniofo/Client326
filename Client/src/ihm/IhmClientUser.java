cd6fdfbaece46aaea28b5a4337455bf34e644153
Antoniofo <antonio57201@gmail.com>
2022-11-14 14:06:33 +0100

    fix

--- Client/src/ihm/IhmClientUser.java
+++ Client/src/ihm/IhmClientUser.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
